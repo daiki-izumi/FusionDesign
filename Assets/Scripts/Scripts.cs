94968d307f82230f845951eb94c18fc06b3c6e1d
123daiki <josephjoester514@gmail.com>
2022-11-14 18:23:20 +0900

    initial comit

---
+++ Assets/Scripts/Scripts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+namespace TextScripts
+{
+    public class Scripts
+    {
+        //セリフを取ってくる関数
+        public string get_line(float now_pos)
+        {
+            Debug.Log("Called from Scripts Class");
+            return "aiueo";
+        }
+        //*デバッグ用*セリフをGoogle スプレッドシートから取ってくる関数
+        public string load_line()
+        {
+            return "kakikukeko";
+        }
+    }
+}
+
---
+++ Assets/Scripts/Story.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TextScripts;
+
+public class Story : MonoBehaviour
+{
+    //スコア
+    int score;
+    //時間
+    float time;
+    //UIのGameObject
+    public GameObject UIs = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(UIs, new Vector3(0.0f, 2.0f, 0.0f), Quaternion.identity);
+        //話者
+        Text speaker = UIs.transform.GetChild(1).GetComponentInChildren<Text>();
+        speaker.text = "Me";
+        //セリフ
+        Text lines = UIs.transform.GetChild(2).GetComponentInChildren<Text>();
+        //lines.text = "hi";
+        //他ファイルからの参照
+        Scripts scr = new Scripts();
+        lines.text = scr.get_line((float)1.0);
+        //lines.text = get_line((float)1.0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //セリフを取ってくる関数
+    string get_line(float now_pos)
+    {
+        Debug.Log("called from Story Class");
+        return "aiueo";
+    }
+}
